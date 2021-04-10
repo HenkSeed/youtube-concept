b8f6fd6f4efbce45d2fb2a7425927d8c72d15bac
HenkSeed <55748514+HenkSeed@users.noreply.github.com>
2021-04-10 22:29:25 +0700

    Добавил два оставшиеся слайдера

--- js/main.js
+++ js/main.js
@@ -1,10 +1,51 @@
 const swiper = new Swiper('.channel-slider', {
 	// Optional parameters
 	loop: true,
-	slidesPerView: 6,
+	slidesPerView: 1,
+	spaceBetween: 20,
+	breakpoints: {
+		1900: { slidesPerView: 6 },
+		1600: { slidesPerView: 5 },
+		1300: { slidesPerView: 4 },
+		1100: { slidesPerView: 3 },
+		800: { slidesPerView: 2 },
+	},
 	// Navigation arrows
 	navigation: {
 		nextEl: '.channel-button-next',
 		prevEl: '.channel-button-prev',
 	},
+});
+
+/* Второй слайдер */
+const swiper2 = new Swiper('.recommended-slider', {
+	// Optional parameters
+	loop: true,
+	slidesPerView: 3,
+	spaceBetween: 20,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.recommended-button-next',
+		prevEl: '.recommended-button-prev',
+	},
+});
+
+/* Третий слайдер */
+const swiper3 = new Swiper('.food_drink-slider', {
+	// Optional parameters
+	loop: true,
+	slidesPerView: 6,
+	spaceBetween: 20,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.food_drink-button-next',
+		prevEl: '.food_drink-button-prev',
+	},
+});
+
+// Настраиваем открытие окна поиска по нажатию на кнопку поиска на малых экранах
+const searchBtn = document.querySelector('.button-search');
+const mobileSearch = document.querySelector('.input-search');
+searchBtn.addEventListener('click', () => {
+	mobileSearch.classList.toggle('is-open');
 });
